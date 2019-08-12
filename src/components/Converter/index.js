9288e40705f42d02186ce31020bb45a66821bd35
Jessie Sidhu <jsidhu22@gmail.com>
2019-08-12 10:05:16 -0700

    progress

--- src/components/Converter/index.js
+++ src/components/Converter/index.js
@@ -1,62 +1,35 @@
 import React, { Component } from 'react';
 import Axios from 'axios';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
+import Dropdown from 'react-bootstrap/Dropdown'
 
 class Converter extends Component {
 
     state = {
-        currencyType: "",
+        currencyID: [],
         currencyVal: ""
 
     };
 
     componentDidMount() {
 
-        this.convertCurrency(10, 'USD', 'PHP', (err, amount) => {
-            console.log(amount);
-          });
+        let apiKey = '9cc6f8323af31d2f1249';
 
-    }
+        // let fromCurrency = encodeURIComponent(fromCurrency);
+        // let toCurrency = encodeURIComponent(toCurrency);
+        // let query = fromCurrency + '_' + toCurrency;
 
-    convertCurrency(amount, fromCurrency, toCurrency, cb) {
-        var apiKey = '9cc6f8323af31d2f1249';
+        // let url = 'https://free.currconv.com/api/v7/convert?q='
+        //     + query + '&compact=ultra&apiKey=' + apiKey;
 
-        fromCurrency = encodeURIComponent(fromCurrency);
-        toCurrency = encodeURIComponent(toCurrency);
-        var query = fromCurrency + '_' + toCurrency;
-
-        var url = 'https://free.currconv.com/api/v7/convert?q='
-            + query + '&compact=ultra&apiKey=' + apiKey;
-
-        Axios.get(url, function (res) {
-            var body = '';
-
-            res.on('data', function (chunk) {
-                body += chunk;
-            });
-
-            res.on('end', function () {
-                try {
-                    var jsonObj = JSON.parse(body);
-
-                    var val = jsonObj[query];
-                    if (val) {
-                        var total = val * amount;
-                        cb(null, Math.round(total * 100) / 100);
-                    } else {
-                        var err = new Error("Value not found for " + query);
-                        console.log(err);
-                        cb(err);
-                    }
-                } catch (e) {
-                    console.log("Parse error: ", e);
-                    cb(e);
-                }
-            });
-        })
-        // .on('error', function (e) {
-        //     console.log("Got an error: ", e);
-        //     cb(e);
-        // });
+        Axios.get(`https://free.currconv.com/api/v7/currencies?apiKey=${apiKey}`)
+            .then(response => {
+                console.log(response);
+                let id = response.data.results;
+                this.state.currencyID.push(id)
+            })
+        console.log('Currency ID:', this.state.currencyID);
     }
 
     handleSubmit = () => {
@@ -70,7 +43,19 @@ class Converter extends Component {
 
                     <div className='row justify-content-sm-center'>
                         <div className='col-sm-12 text-center'>
-
+                            <ButtonToolbar>
+                                {[DropdownButton].map((DropdownType, idx) => (
+                                    <DropdownType
+                                        size="sm"
+                                        variant="secondary"
+                                        title="Drop small"
+                                        id={`dropdown-button-drop-${idx}`}
+                                        key={idx}
+                                    >
+                                        <Dropdown.Item eventKey={idx}>{this.state.currencyID[0].keys}</Dropdown.Item>
+                                    </DropdownType>
+                                ))}
+                            </ButtonToolbar>
                         </div>
                     </div>
 
