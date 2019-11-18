40c7d24fec47d151a4d8dc15078e2cda237b41ff
Luis Arevalo <arevaloluis89@yahoo.com>
2019-11-17 19:44:11 -0800

    Updates

---
+++ precache-manifest.fdfe31e5570785593b9fd03ffd1cb922.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dda22e2324a9ff1f12d173d0cdc1c7c",
+    "url": "/pearl/index.html"
+  },
+  {
+    "revision": "231ab15218f36683325d",
+    "url": "/pearl/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "375ffb846c17d7a831c6",
+    "url": "/pearl/static/css/main.41e61244.chunk.css"
+  },
+  {
+    "revision": "231ab15218f36683325d",
+    "url": "/pearl/static/js/2.4c9231af.chunk.js"
+  },
+  {
+    "revision": "375ffb846c17d7a831c6",
+    "url": "/pearl/static/js/main.a4f002e4.chunk.js"
+  },
+  {
+    "revision": "8309d3fe529eb49e8d8d",
+    "url": "/pearl/static/js/runtime~main.3a918140.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.77f559e7fe0de273e4cb5fc270cdee86.js"
+  "/pearl/precache-manifest.fdfe31e5570785593b9fd03ffd1cb922.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pearl/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
